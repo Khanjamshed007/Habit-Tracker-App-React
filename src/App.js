168fb59f61a482c5dd8883e2cd2a9d4f2b9ce964
jamshed khan <khanjamshed404@gmail.com>
2023-05-30 18:26:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import AddHabit from "./components/AddHabit/AddHabit";
+import Weekview from "./components/Weekview/Weekview";
+import { useSelector } from "react-redux";
+import List from "./components/List/List";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
+  let habits=useSelector((state)=>state['habits'])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {/* ToastContainer settings */}
+      <ToastContainer
+        position="top-right"
+        autoClose={800}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored" />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<List habits={habits} />} />
+          <Route path="/add" element={<AddHabit />} />
+          <Route path="/week-view" element={<Weekview />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
